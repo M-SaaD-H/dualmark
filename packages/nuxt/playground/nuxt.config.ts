c91a90159c31ef9b02dd09a5e878667355750ca8
Saad Haider <mohd.saadhaider@gmail.com>
2026-05-18 10:18:46 +0530

    Initial commit

---
+++ packages/nuxt/playground/nuxt.config.ts
@@ -0,0 +1,8 @@
+import { defineNuxtConfig } from 'nuxt/config'
+import DualmarkNuxt from '../src/module'
+
+export default defineNuxtConfig({
+  modules: [DualmarkNuxt],
+  devtools: { enabled: true },
+  compatibilityDate: 'latest',
+})
